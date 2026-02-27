f1016d884350d08eec6d7d1dda6f6a82fab6a8ef
Sudip Dahal <sudip@ooo.sudipdahal1650.com.np>
2026-02-27 20:31:50 +0545

    change

--- playground/views.py
+++ playground/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 def say_hello(request):
     x = 1
     y = 2
-    return render(request, "hello.html", {'name':"sudip"})
+    return render(request, "hello.html", {'name':"sudip1"})
