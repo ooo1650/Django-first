0c324cbd858517bcae832ab955bee770de93cf3f
Sudip Dahal <sudip@ooo.sudipdahal1650.com.np>
2026-03-01 00:27:57 +0545

    using need

--- playground/views.py
+++ playground/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 def say_hello(request):
     x = 1
     y = 2
-    return render(request, "hello.html", {'name':"ooo"})
+    return render(request, "hello.html", {'name':"ooo1"})
